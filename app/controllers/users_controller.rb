5dbb2c34444092b8b4fd067265893a684cbd78fb
Gustavo Canedo <gknedo@gmail.com>
2016-07-02 13:24:08 -0300

    added: signup error messages and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
     else
       render 'new'
     end
